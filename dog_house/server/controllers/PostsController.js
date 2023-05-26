f6c0134bfd6802a205cdbc17390faa2edf276579
Jacob Winfrey <jacob_winfrey@yahoo.com>
2023-05-26 11:31:41 -0600

    commit

---
+++ dog_house/server/controllers/PostsController.js
@@ -0,0 +1,35 @@
+import { postsService } from "../services/PostsService.js"
+import BaseController from "../utils/BaseController.js";
+import { Auth0Provider } from "@bcwdev/auth0provider";
+export class PostsController extends BaseController {
+constructor() {
+  super('api/posts')
+  this.router
+  .get('', this.getPosts)
+  // add in the .use to get userInfo attached to post (plus do extra obv)
+  .use(Auth0Provider.getAuthorizedUserInfo)
+  .post('', this.createPost)
+}
+
+async createPost(req,res,next) {
+  try {
+    req.body.creatorId = req.userInfo.id
+    const postData = req.body
+    const newPost = await postsService.createPosts(postData)
+    return res.send(newPost)
+  } catch (error) {
+    next(error)
+    
+  }
+}
+
+async getPosts( req, res, next) {
+  try {
+    const query = req.query
+    const posts = await postsService.getPosts(query)
+    return res.send(posts)
+  } catch (error) {
+    next(error)
+  }
+}
+}
---
+++ dog_house/server/models/Post.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+  export const PostSchema = new Schema(
+    {
+      name: { type: String, required: true},
+      breed: { type: String, required: true},
+      age: {type: Number, required: true},
+      size: {type: String, required: true, Enum: ['small', 'medium','large'] },
+      creatorId: {type: Schema.Types.ObjectId, required: true, ref: 'Account' }
+  },
+  { timestamps: true, toJSON: {virtuals: true}}
+  )
+  PostSchema.virtual('creator',{
+    localField: 'creatorId',
+    foreignField: '_id',
+    
+    justOne: true,
+    ref: 'Account'
+  })
